a29940528784702244e861fe64594e70df0a6ad0
Robert <robertclark0@gmail.com>
2016-10-07 15:06:06 -0500

    update

--- core-components/analysis/controllers/dataFilter.contoller.js
+++ core-components/analysis/controllers/dataFilter.contoller.js
@@ -10,17 +10,30 @@
     $scope.filters = SF.availableDataFilters();
     $scope.canvasFilters = SF.canvasDataFilters();
 
+    $scope.operations = ["Range", "Equal", "Toggle", "Between", "Greater", "Less", "Greater or Equal", "Less or Equal" ]
+
     $scope.filterName = function (filter) {
         return filter.name + " ..." + filter.GUID.substr(32,4);
     };
 
     $scope.selectedModel = null;
     $scope.disabled = true;
+    $scope.allias = null;
+    $scope.selectedData = null;
+    $scope.selectedOperation = null
+    $scope.selectedOperations = [];
 
-    $scope.checkSelection = function () {
 
-        console.log($scope.selectedModel);
+    $scope.checkTypeSelection = function () {
+        $scope.allias = $scope.selectedModel.name;
     };
+    $scope.checkDataSelection = function () {
+        $scope.allias = $scope.selectedData.COLUMN_NAME;
+    };
+    $scope.addOperation = function () {
+        $scope.selectedOperations.push({ operation: $scope.selectedOperation, useData: false });
+        $scope.selectedOperation = null
+    }
 
 
     $scope.closeDialog = function () {
