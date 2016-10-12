7d55275ec4c78cbcb6e91ce0cc987a9be4b1428d
Robert <robertclark0@gmail.com>
2016-10-12 14:50:27 -0500

    updates

--- assets/js/dist.analysis.module.js
+++ assets/js/dist.analysis.module.js
@@ -366,39 +366,65 @@ analysis.controller('Analysis', ['$scope', 'appManager', '$state', '$interval', 
 analysis.controller('DataFilter', ['$scope', 'appManager', 'componentViewFactory', '$mdDialog', function ($scope, appManager, componentViewFactory, $mdDialog) {
 
     // ---- ---- ---- ---- Controller and Scope variables ---- ---- ---- ----   
-    //$scope.SF = appManager.state.SF;
-    //$scope.DSO = appManager.state.DSO;
-    //$scope.DO = appManager.data.DO;
     var SF = appManager.state.SF;
     $scope.DO = appManager.data.DO;
-
     $scope.filters = SF.availableDataFilters();
     $scope.canvasFilters = SF.canvasDataFilters();
+    $scope.componentProperties = componentViewFactory.componentProperties;
 
-    $scope.operations = ["Range", "Equal", "Toggle", "Between", "Greater", "Less", "Greater or Equal", "Less or Equal" ]
+    $scope.operations = ["Range", "Equal", "Toggle", "Between", "Greater", "Less", "Greater or Equal", "Less or Equal"]
 
-    $scope.filterName = function (filter) {
-        return filter.name + " ..." + filter.GUID.substr(32,4);
-    };
-
-    $scope.selectedModel = null;
     $scope.disabled = true;
-    $scope.allias = null;
-    $scope.selectedData = null;
     $scope.selectedOperation = null
-    $scope.selectedOperations = [];
+
+    $scope.newFilter = {
+        model: null,
+        allias: null,
+        dataValue: null,
+        operations: []
+    };
 
 
     $scope.checkTypeSelection = function () {
-        $scope.allias = $scope.selectedModel.name;
+        $scope.newFilter.allias = $scope.newFilter.model.name;
     };
     $scope.checkDataSelection = function () {
-        $scope.allias = $scope.selectedData.COLUMN_NAME;
+        $scope.newFilter.allias = $scope.newFilter.dataValue.COLUMN_NAME;
     };
     $scope.addOperation = function () {
-        $scope.selectedOperations.push({ operation: $scope.selectedOperation, useData: true });
+        $scope.newFilter.operations.push({ operation: $scope.selectedOperation, useData: true });
         $scope.selectedOperation = null
     }
+    $scope.saveFilter = function () {
+        if ($scope.newFilter.model.type === 'custom') {
+            var newGUID = SF.generateGUID();
+            var dataGroupReference = { GUID: newGUID, selectedValues: [] };
+            var filter = angular.copy($scope.newFilter);
+            filter.GUID = newGUID;
+
+            $scope.componentProperties.editObject.filters.push(dataGroupReference);
+        }
+        else {
+
+        }
+    };
+
+    $scope.$watch('newFilter', function (nv) {
+        if (nv.model) {
+            if (nv.model.type === 'custom') {
+                if (nv.dataValue !== null && nv.operations.length > 0) {
+                    $scope.disabled = false;
+                }
+                else {
+                    $scope.disabled = true;
+                }
+            }
+            else{
+                $scope.disabled = false;
+            }
+        }
+
+    }, true);
 
 
     $scope.closeDialog = function () {
